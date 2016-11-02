5bb4e0b973dee2df3e935c53577e4d12de7f50bc
Bruno Mermet <Bruno.Mermet@univ-lehavre.fr>
2016-11-02 09:30:46 +0100

    Rajout de la classe Cercle.

---
+++ Cercle.java
@@ -0,0 +1,12 @@
+public class Cercle {
+    private int rayon;
+
+    public Cercle(int monRayon) {
+	rayon = monRayon;
+    }
+
+    @Override
+    public String toString() {
+	return "Cercke [" + rayon + "]";
+    }
+}
