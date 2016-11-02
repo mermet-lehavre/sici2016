8eb8ccd17b2777b620400611f3fb914361b3a8cb
Bruno Mermet <Bruno.Mermet@univ-lehavre.fr>
2016-11-02 10:26:52 +0100

    On passe maintenant par un tableau dans Main.

--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
 public class Main {
     public static void main(String... args) {
-	Figure r1 = new Rectangle(4,5);
-	System.out.println(r1);
-	Figure c1 = new Cercle(5);
-	System.out.println(c1);
-	Figure t1 = new Triangle(3,4,5);
-	System.out.println(t1);
+	Figure[] f;
+	f = new Figure[3];
+	f[0] = new Rectangle(4,5);
+	System.out.println(f[0]);
+	f[1] = new Cercle(5);
+	System.out.println(f[1]);
+	f[2] = new Triangle(3,4,5);
+	System.out.println(f[2]);
     }
 }
