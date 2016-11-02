fdbbbf7b1147607e0c1770d679efae37ab13a393
Bruno Mermet <Bruno.Mermet@univ-lehavre.fr>
2016-11-02 09:17:24 +0100

    Séparation du main.

--- Rectangle.java
+++ Rectangle.java
@@ -6,9 +6,4 @@ public class Rectangle {
 	largeur = larg;
 	longueur = lg;
     }
-
-    public static void main(String... args) {
-	Retangle r1 = new Rectangle(4,5);
-	System.out.println(r1);
-    }
 }
